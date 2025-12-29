55d8fc1eaed39b679cf5307f207bbe0be4b826ea
agalya-lr <agalya.l@qsignage.com>
2025-12-29 21:03:15 +0530

    pose.ts is moved from mediapipe folder to components folder

---
+++ src/components/pose.ts
@@ -0,0 +1,17 @@
+import { Pose } from "@mediapipe/pose"
+
+// Use LOCAL files from public/mediapipe/pose - more reliable than CDN
+// This avoids "Aborted (Assertion failed)" errors and works offline
+export const pose = new Pose({
+  locateFile: (file) => `/mediapipe/pose/${file}`,
+})
+
+pose.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5,
+})
+
+
+
