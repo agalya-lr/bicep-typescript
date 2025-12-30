545546595bd16967b93356d72b74beb02b490106
agalya-lr <agalya.l@qsignage.com>
2025-12-30 10:48:44 +0530

    comments added

--- src/components/smooth.ts
+++ src/components/smooth.ts
@@ -1,4 +1,5 @@
 export function smooth(value: number, prev: number, alpha = 0.2) {
     return alpha * value + (1 - alpha) * prev
   }
-  
+  //removes noise from the angle
+  //alpha is the smoothing factor
