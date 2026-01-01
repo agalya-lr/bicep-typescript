ae4897447816d16e236a0ae2e8ccdfcf6f39be8a
agalya-lr <agalya.l@qsignage.com>
2026-01-01 10:58:19 +0530

    scroll bar removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import BicepCounter from './BicepCounter'
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh', background: '#000' }}>
+    <div style={{ 
+      width: '100vw', 
+      height: '100vh', 
+      background: '#000',
+      overflow: 'hidden',
+      margin: 0,
+      padding: 0,
+      boxSizing: 'border-box'
+    }}>
       <BicepCounter />
     </div>
   )
